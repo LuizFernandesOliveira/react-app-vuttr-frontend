1527a04a78553ea3db5ac657098a7052a8a036d8
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-01-29 21:03:48 -0300

    criei o test do header e o implementei

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,28 @@
+import {
+  Container,
+  Content,
+  Title,
+  Subtitle,
+  Wrapper,
+} from './styles';
+
+function Header() {
+  return (
+    <Container>
+      <Title>
+        VUTTR
+      </Title>
+
+      <Subtitle>
+        Very Useful Tools to Remember
+      </Subtitle>
+
+      <Content>
+        <Wrapper>
+
+        </Wrapper>
+      </Content>
+    </Container>
+  );
+}
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-bottom: 10px;
+`;
+
+export const Content = styled.div``;
+
+export const Title = styled.h1``;
+
+export const Subtitle = styled.h3``;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+`;
