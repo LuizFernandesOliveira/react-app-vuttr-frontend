005d458de76e363c5c7781d392fd839a93030e47
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-02-17 12:18:29 -0300

    criei os inputs da tela de adicionar tools

--- src/components/CreateTools/index.js
+++ src/components/CreateTools/index.js
@@ -1,11 +1,19 @@
-import PropTypes from 'prop-types'
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 import {
+  ButtonSubmit,
   Container,
   Content,
+  Form,
   Header,
   IconAddImg,
   IconCloseImg,
+  InputLink,
+  InputName,
+  InputTags,
+  Label,
+  TextareaDescription,
   Title,
 } from './styles';
 
@@ -13,6 +21,18 @@ import IconClose from '../../assets/icons/Icon-Close-2px.svg';
 import IconAdd from '../../assets/icons/iconadd.svg';
 
 const CreateTools = ({ toggle, setToggle }) => {
+  const [inputs, setInputs] = useState({
+    title: '', link: '', description: '', tags: '',
+  });
+
+  const changeInputs = ({ target }) => {
+    const {name, value} = target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
   return (
     <Container toggle={toggle} data-testid="create-tools-container">
       <Content>
@@ -28,6 +48,62 @@ const CreateTools = ({ toggle, setToggle }) => {
             onClick={ () => setToggle(!toggle) }
           />
         </Header>
+        <Form>
+          <Label>
+            Tool Name
+            <InputName
+              required
+              type="text"
+              name="title"
+              onChange={ changeInputs }
+              value={ inputs.title }
+              data-testid="create-tool-title"
+            />
+          </Label>
+
+          <Label>
+            Tool Link
+            <InputLink
+              required
+              type="text"
+              name="link"
+              onChange={ changeInputs }
+              value={ inputs.link }
+              data-testid="create-tool-link"
+            />
+          </Label>
+
+          <Label>
+            Tool Description
+            <TextareaDescription
+              required
+              name="description"
+              rows="5"
+              onChange={ changeInputs }
+              value={ inputs.description }
+              data-testid="create-tool-description"
+            >
+            </TextareaDescription>
+          </Label>
+
+          <Label>
+            Tags
+            <InputTags
+              required
+              type="text"
+              name="tags"
+              onChange={ changeInputs }
+              value={ inputs.tags }
+              data-testid="create-tool-tags"
+            />
+          </Label>
+
+          <ButtonSubmit
+            data-testid="create-tool-btn-add"
+          >
+            Add tool
+          </ButtonSubmit>
+        </Form>
       </Content>
     </Container>
   );
