6919edd49720ed41451a8323d6096e0bafb4fb74
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-02-17 19:38:05 -0300

    criei o item para ferramentas

---
+++ src/tests/components/mockData.js
@@ -0,0 +1,16 @@
+export const tool = {
+  id: 1,
+  title: 'github',
+  link: 'https://github.com',
+  description: 'ferramenta de versionamento de código',
+  tags: 'development production',
+};
+
+export const toolRender = {
+  id: 1,
+  title: 'github',
+  link: 'https://github.com',
+  description: 'ferramenta de versionamento de código',
+  tags: ['development', 'production'],
+};
+
