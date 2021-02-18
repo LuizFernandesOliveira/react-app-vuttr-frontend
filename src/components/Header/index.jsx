06d301a4719d4d570e3b5e2cb1239822098d8f9b
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-02-18 15:18:56 -0300

    implementei a busca

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-
+import {getToolsByTag, getToolsByTitle} from '../../store/tools/action';
 import CreateTools from '../CreateTools';
+import {useDispatch} from 'react-redux';
 
 import {
   Container,
@@ -20,7 +21,17 @@ import IconAdd from '../../assets/icons/iconadd.svg';
 import IconLogo from '../../assets/icons/icon-Settings-Tool.svg';
 
 function Header() {
+  const dispatch = useDispatch();
   const [toggle, setToggle] = useState(false);
+  const [checked, setChecked] = useState(false);
+  const [word, setWord] = useState('');
+
+  const changeSearch = async (value) => {
+    await setWord(value);
+    if (checked) dispatch(getToolsByTag(value));
+    else dispatch(getToolsByTitle(value));
+  };
+
   return (
     <Container>
       <Title>
@@ -39,12 +50,15 @@ function Header() {
               data-testid="input-search"
               type="text"
               placeholder="search"
+              value={word}
+              onChange={({target}) => changeSearch(target.value)}
             />
 
             <LabelCheckbox>
               <CheckboxInput
                 type="checkbox"
                 data-testid="input-checkbox"
+                onChange={({target}) => setChecked(target.checked)}
               />
               search in tags only
             </LabelCheckbox>
